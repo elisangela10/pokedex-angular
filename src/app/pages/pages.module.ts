29016fd4ba3bec75f69741f713d15b5c0053ddf6
elisangela10 <elisangela.miranda07@bol.com.br>
2021-10-13 18:24:59 -0300

    modules shared | home

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,6 +6,7 @@ import { RoutingModule } from './routing.module';
 //pages
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { DetailsComponent } from './details/details.component';
   ],
   imports: [
     CommonModule,
-    RoutingModule
+    RoutingModule,
+    SharedModule
   ]
 })
 export class PagesModule { }
